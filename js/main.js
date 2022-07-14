f7192d64b6c82909911316770a64b05fda76dc21
aliceandherdolls <alicjagorkiewicz92@gmail.com>
2022-07-14 17:10:09 +0200

    firsts

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let num1 = 12;
+let num2 = 7;
+
+
+
+if (num1 == 20) {
+    console.log('true');
+} else if (num2 == 20) {
+    console.log('true');
+} else if (num1 + num2 == 20) {
+    console.log('true');
+} else if (num1 + num2 < 20) {
+    console.log('true');
+}
