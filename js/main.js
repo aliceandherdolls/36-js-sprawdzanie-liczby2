89fc5d84a5be3f521f8a92683c3413c7642f1ca3
aliceandherdolls <alicjagorkiewicz92@gmail.com>
2022-07-18 13:29:52 +0200

    function

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 let num1 = 12;
 let num2 = 7;
 
+function action(par1,par2) {
+    if (par1 === 20) {
+        console.log('true');
+    } else if (par2 === 20) {
+        console.log('true');
+    } else if (par1 + par2 <= 20) {
+        console.log('true');
+    } 
 
-
-if (num1 == 20) {
-    console.log('true');
-} else if (num2 == 20) {
-    console.log('true');
-} else if (num1 + num2 == 20) {
-    console.log('true');
-} else if (num1 + num2 < 20) {
-    console.log('true');
 }
+
+action(12,7);
+
+
