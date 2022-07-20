a045089ce1d0b8de3806533bc23690083d51d2b2
aliceandherdolls <alicjagorkiewicz92@gmail.com>
2022-07-20 12:02:35 +0200

    return

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
-let num1 = 12;
-let num2 = 7;
 
-function action(par1,par2) {
+function action(par1, par2) {
+    let sum = par1 + par2;
+
+
     if (par1 === 20) {
-        console.log('true');
+        return ('true');
     } else if (par2 === 20) {
-        console.log('true');
-    } else if (par1 + par2 <= 20) {
-        console.log('true');
-    } 
+        return ('true');
+    } else if (sum <= 20) {
+        return ('true');
+    }
 
 }
 
-action(12,7);
+console.log(action(12, 7));
 
 
